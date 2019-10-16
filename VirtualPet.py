453d0e7046619d9a3b1ab7ed81ebe2b689c5c48f
odonkin <odonkin@storans.school.nz>
2019-10-17 12:00:18 +1300

    Testing

---
+++ VirtualPet.py
@@ -0,0 +1,18 @@
+# This is a program that allows for kids to have a virtual pet that they can name, eat and do exercise with.
+
+#Pet Dictionary
+pets = ['Dog', 'Cat', 'Fish', 'Rabbit']
+
+#Food Values
+#food = {"Kibble": 0.1, "Flakes": 0.1, "Lettuce":0.3, ""}
+
+
+# Firstly a menu
+
+print("Welcome to Virtual Pet!")
+print()
+print(input("Please choose an animal to be your virtual pet: "))
+
+
+
+#
