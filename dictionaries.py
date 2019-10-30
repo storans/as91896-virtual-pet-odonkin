384ab16f44049955343ca282866dc0bd20efa539
odonkin <odonkin@storans.school.nz>
2019-10-30 13:33:30 +1300

    Dictionaires 1

--- dictionaries.py
+++ dictionaries.py
@@ -1,6 +1,3 @@
-# This is a program that allows for kids to have a virtual pet
-# That they can name, eat and do exercise with.
-
 # Dictionaries for pet activities
 PET_ACTIVITY = {"exercise": 2, "feed": 1}
 
@@ -9,11 +6,4 @@ exercises = {"park playdate": 0, "beach time": 0, "jumbo jog":0}
 exercise_calories = {"park playdate": 0.2, "beach time": 0.3, "jumbo jog":0.4}
 
 # Food options for pet (dictionary)
-food_list = {"kibble": 1, "carrot":2, "canned food":3, "seaweed":4, "vegetables": 5, "water":6}
-
-
-
-
-
-
-
+food_list = {"kibble": 3, "carrot":2, "canned food":4, "seaweed":2, "vegetables": 3, "water":1}
