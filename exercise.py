2480a4f055ebabcae21ec487e7295d0d7dac4906
odonkin <odonkin@storans.school.nz>
2019-10-30 14:02:32 +1300

    Second idea - High Low working - Options not working - Program works up until after you pick an option

--- exercise.py
+++ exercise.py
@@ -15,12 +15,12 @@ def check_integer(question, low, high, error):
                 print(error)
                 print()
 
-        # If an invalid input is made, the prompt comes up again
+ # If an invalid input is made, the prompt comes up again
         except ValueError:
             print(error)
 
 # Give the user to pick whether they want to exercise or feed their pet.
-pet_activities = input("Would you like to exercise or feed {}?: ".format(pet_name)).strip().lower()
+pet_activities = input("Would you like to exercise or feed your pet?: ").strip().lower()
 
 weight = 35
 
@@ -33,7 +33,7 @@ while True:
   "3. Enter Jumbo Jog\n"
   "4. Exit\n")
 
-  option = check_integer("Please enter number of the choices above:")
+  option = check_integer("Please enter number of the choices above:", 1, 4, "This is not an option")
 
   if option == 1 or option == 2 or option == 3:
     activity = choose_activity(option)
