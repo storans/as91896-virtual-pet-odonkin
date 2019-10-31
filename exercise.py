91429e68fe39920afbe7da216f983e57c5b424ae
odonkin <odonkin@storans.school.nz>
2019-10-31 13:18:46 +1300

    Improvement ORIGINAL

--- exercise.py
+++ exercise.py
@@ -2,12 +2,13 @@
 PET_ACTIVITY = {"exercise": 2, "feed": 1}
 
 # Pets exercise options (dictionary)
-exercises = {"park playdate": 1, "beach time": 2, "jumbo jog": 3}
+exercises_dict = {"park play date": 1, "beach time": 2, "jumbo jog": 3}
+exercises_list = ['park play date', 'beachtime', 'jumbo jog']
 
 
-# pet_weight = 35
+pet_weight = 1.9
 
-#pet_weight -= exercises
+# pet_weight -= exercises
 
 def choose_activity(pet_weight):
     activity_menu = []
@@ -15,23 +16,38 @@ def choose_activity(pet_weight):
     if activity_choice == "exercise":
         print("Awesome, let's have a look at the exercises!")
 
-        print("1. Enter Park Playdate\n"
-              "2. Enter Beach Time\n"
-              "3. Enter Jumbo Jog\n"
-              "4. Exit\n")
 
-        option = check_integer("Please enter number of the choices above:", 1, 4, "This is not an option")
+# Function for showing pet weight
+# Function to allow user to choose a food and feed pet said food
+def user_choice_exercise(exercises_list, exercise_dict):
+    initial_weight = 2
+    low = 1
+    high = 3
+    activity_menu = []
+    print("What would you like to do with your pet?:")
 
-        pet_activity = activity_menu[option - 1]
+    number = 1
+    for action in exercises:
+        print("{}.  {}".format(number, exercises))
+        number += 1
+        activity_menu.append(exercises)
 
-        #if option == 1:
-           # pet_activity = "park playdate"
-            #print(pet_activity)
-       # elif option == 2:
-          #  activity = "beach time"
-        #else:
-           # activity = "jumbo jog"
-        #return activity
+    choice = check_integer("Please choose the number with the activity you want to do with your pet.", low, high, "Please enter a number")
+
+    chosen_exercise = activity_menu[choice - 1]
+
+    print("You will take your pet and you guys will {}.".format(chosen_exercise.title()))
+
+    initial_weight += exercises[chosen_exercise]
+    # print(pet_activity)
+    # if option == 1:
+    # pet_activity = "park playdate"
+    # print(pet_activity)
+    # elif option == 2:
+    #  activity = "beach time"
+    # else:
+    # activity = "jumbo jog"
+    # return activity
 
 
 def check_integer(question, low, high, error):
@@ -51,29 +67,30 @@ def check_integer(question, low, high, error):
 
 
 # Give the user to pick whether they want to exercise or feed their pet.
-#pet_activities = input("Would you like to exercise or feed your pet?: ").strip().lower()
+# pet_activities = input("Would you like to exercise or feed your pet?: ").strip().lower()
 
 weight = 35
 
 # This allows the user to see the options and pick one they would like to do if they choose to exercise their pet
-
-while True:
+alive = True
+while alive:
 
     choose_activity(weight)
-    #if option == 1 or option == 2 or option == 3:
-        #activity = choose_activity(weight)
-       # print(weight)
-       # print("Your pet now weights {}".format(weight))
-       # if weight > 200:
-         #   print("Your pet has not had enough exercise! It has passed away!")
-         #   break
-3
-       # elif weight < 5:
-        #    print("You pet has done too much exercise and not had enough to eat, it has passed away!")
-         #   break
+    option = user_choice_exercise(exercises_dict)
+    if option == 1 or option == 2 or option == 3:
+        activity = choose_activity(weight)
+        print(weight)
+        # print("Your pet now weights {}".format(weight))
+        # if weight > 200:
+        # print("Your pet has not had enough exercise! It has passed away!")
+        #   break
 
-       # else:
-          #  print("At the moment, your pet is healthy! Good Job! :)")
+    # elif weight < 5:
+    #    print("You pet has done too much exercise and not had enough to eat, it has passed away!")
+    #   break
 
-#else:
-    #print("Would you like to quit the game?")
+    # else:
+    #  print("At the moment, your pet is healthy! Good Job! :)")
+
+# else:
+# print("Would you like to quit the game?")
---
+++ exercise2.py
@@ -0,0 +1,62 @@
+# Dictionaries for pet activities
+PET_ACTIVITY = {"exercise": 2, "feed": 1}
+
+# Pets exercise options (dictionary)
+exercises = {"park playdate": 1, "beach time": 2, "jumbo jog":3}
+
+#def choose_activity()
+
+def check_integer(question, low, high, error):
+    valid = False
+    while not valid:
+        try:
+            number = int(input("{}".format(question)))
+            if low <= number <= high:
+                return number
+            else:
+                print(error)
+                print()
+
+ # If an invalid input is made, the prompt comes up again
+        except ValueError:
+            print(error)
+
+# Give the user to pick whether they want to exercise or feed their pet.
+pet_activities = input("Would you like to exercise or feed your pet?: ").strip().lower()
+
+weight = 35
+
+def exercise_pet(activity, pet_weight):
+    pet_weight -= exercises[activity]
+    return pet_weight
+
+# This allows the user to see the options and pick one they would like to do if they choose to exercise their pet
+if exercises == "exercise":
+  print("Awesome, let's have a look at the exercises!")
+while True:
+  print("1. Enter Park Playdate\n"
+  "2. Enter Beach Time\n"
+  "3. Enter Jumbo Jog\n"
+  "4. Exit\n")
+
+  option = check_integer("Please enter number of the choices above:", 1, 4, "This is not an option")
+
+  if option == 1:
+      activity = "park playdate"
+  elif option == 2:
+      activity = "beachtime"
+
+        activity =  "jumbo jog"
+
+pet_weight = exercise_pet(activity, pet_weight)
+    print("Your pet now weights {}".format(end_weight))
+    if end_weight > 200:
+      print("Your pet has not had enough exercise! It has passed away!")
+      break
+
+    elif end_weight < 5:
+      print("You pet has done too much exercise and not had enough to eat, it has passed away!")
+      break
+
+    else:
+        print("At the moment, your pet is healthy! Good Job! :)")
