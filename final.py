b9b592bd80b2721662fc6d411582a97c61a625ab
odonkin <odonkin@storans.school.nz>
2019-11-25 10:28:45 +1300

    This includes a 2D list and has a new way of asking the child whether they would like to choose their own name or select a default name.

--- final.py
+++ final.py
@@ -3,6 +3,9 @@
 # This game is not a correct way to look after a pet BUT has some good attributes in taking care of a pet.
 # Also good for kids maths development as they can use it to help them work our adding and subtracting
 
+#Allows for a random name to be selected in a later component
+import random
+
 # Function to check whether the choice is an option in feed and exercise
 def check_integer(question, low, high, error):
     valid = False
@@ -42,53 +45,69 @@ EXERCISES = {"park playdate": 1, "beach time": 2, "jumbo jog":3}
 # Food options for pet (dictionary)
 FOOD = {"water":1, "vegetables": 2,"canned food":3}
 
-# Prints welcome screen
-print("Welcome to Virtual Pet!")
-
-print("Create your pet, exercise and feed it!")
-print("But make sure your taking care of it properly! If goes under 2kgs or over 30kgs, it will die!")
-print("Have fun, enjoy and lets get started!")
-print()
-# Prints pet list
-print(PETS)
-
-# This allows the user to input what animal they would like as their virtual pet
-virtual_pet = input("Please choose an animal off the list to be your virtual pet: ").strip().lower()
-
-# This tells the user when the pet they have entered is okay in the program and part of the pet list.
-if virtual_pet in PETS:
-  print("Congratulations! You have chosen a {} as a Virtual Pet!".format(virtual_pet))
-
- # This tells the user the animal they have chosen is not allowed in the program as it is not in the list and allows them to pick again.
-else:
-  print("Sorry, {} is not an option in this Virtual Pet game today :) Try another from our list!".format(virtual_pet))
-  virtual_pet = input("Please choose an animal off the list to be your virtual pet: ").strip().lower()
-
-# This allows the user to input a name to personalise their animal.
-pet_name = input("Please choose a name for your {}: ".format(virtual_pet).strip().title())
-
-# Show the user their animal and name
-print("Awesome! You now own a {} named {}! ".format(virtual_pet, pet_name))
-
-#Allows user to make sure they are entering a number with digits not with letters
-# Creates loop if it does not fit the criteria
-while True:
-  try:
-    weight = int(input("Please enter a weight in kgs for your pet between 2 and 30: "))
-    if weight > 30:
-      print("This is not an available weight!")
-    elif weight < 2:
-      print("This is not an available weight")
-    else:
-      print("Awesome!")
-       # Prints animals name and weight
-      print("{}'s weight is {} kgs.".format(pet_name, weight))
-      break
-  except ValueError:
-    print("That was not a valid weight or number, please enter a weight between 2 and 30!")
+pet_names = [["Cojo", "Barney"],["Greg", "Dog"],["Darius","Hop"],["Craig","Zoom"]]
 
 # This is the start of the main routine.
 if __name__ == "__main__":
+    # Prints welcome screen
+    print("Welcome to Virtual Pet!")
+
+    print("Create your pet, exercise and feed it!")
+    print("But make sure your taking care of it properly! If goes under 2kgs or over 30kgs, it will die!")
+    print("Have fun, enjoy and lets get started!")
+    print()
+    # Prints pet list
+    print(PETS)
+
+    # This allows the user to input what animal they would like as their virtual pet
+    virtual_pet = input("Please choose an animal off the list to be your virtual pet: ").strip().lower()
+
+    # This tells the user when the pet they have entered is okay in the program and part of the pet list.
+    if virtual_pet in PETS:
+      print("Congratulations! You have chosen a {} as a Virtual Pet!".format(virtual_pet))
+
+     # This tells the user the animal they have chosen is not allowed in the program as it is not in the list and allows them to pick again.
+    else:
+      print("Sorry, {} is not an option in this Virtual Pet game today :) Try another from our list!".format(virtual_pet))
+      virtual_pet = input("Please choose an animal off the list to be your virtual pet: ").strip().lower()
+
+    print("Would you like to choose your own name or use a default name?")
+    pet_name = int(input("Type 1 for default name and 2 to choose your own: "))
+    if pet_name == 1:
+        if virtual_pet == "dog":
+            pet_name = random.choice(pet_names[0])
+        elif virtual_pet == "cat":
+            pet_name = random.choice(pet_names[1])
+        elif virtual_pet == "bunny":
+            pet_name = random.choice(pet_names[2])
+        else:
+            pet_name = random.choice(pet_names[3])
+    else:
+        # This allows the user to input a name to personalise their animal.
+         pet_name = input("Please choose a name for your {}: ".format(virtual_pet)).strip().title()
+
+
+    # Show the user their animal and name
+    print("Awesome! You now own a {} named {}! ".format(virtual_pet, pet_name))
+
+    #Allows user to make sure they are entering a number with digits not with letters
+    # Creates loop if it does not fit the criteria
+    while True:
+      try:
+        weight = int(input("Please enter a weight in kgs for your pet between 2 and 30: "))
+        if weight > 30:
+          print("This is not an available weight!")
+        elif weight < 2:
+          print("This is not an available weight")
+        else:
+          print("Awesome!")
+           # Prints animals name and weight
+          print("{}'s weight is {} kgs.".format(pet_name, weight))
+          break
+      except ValueError:
+        print("That was not a valid weight or number, please enter a weight between 2 and 30!")
+
+
     # First is a variable set to True to check whether the program should continue running.
     tracking_on = True
     while tracking_on == True:
